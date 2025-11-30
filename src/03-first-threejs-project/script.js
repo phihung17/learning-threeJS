18658824365111c8450186c7c4c256b187f70874
phihung17 <hung01092003@gmail.com>
2025-11-30 13:35:27 +0700

    03. First Three.js Project

---
+++ src/03-first-threejs-project/script.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+//PerspectiveCamera(fov?: number (field of view), aspect?: number (width / height), near?: number (near clipping plane), far?: number (far clipping plane))
+const camera = new THREE.PerspectiveCamera(50, sizes.width / sizes.height)
+camera.position.z = 2
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
